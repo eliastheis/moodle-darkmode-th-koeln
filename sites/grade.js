f9dd42090a7438e0e4dff2963501c4aed27ce56f
eliastheis <eliastheis.sg@gmail.com>
2021-01-07 23:01:13 +0100

    Added darkmode for /grade

---
+++ sites/grade.js
@@ -0,0 +1,18 @@
+console.log("[DarkMode] loading grade.js...");
+
+elem = document.getElementsByTagName("thead")[0];
+addDefaultDesign(elem);
+
+elems = document.getElementsByTagName("th");
+for (let i = 0; i < elems.length; i++) {
+	addDefaultDesign(elems[i])
+}
+
+elems = document.getElementsByClassName("item");
+for (let i = 0; i < elems.length; i++) {
+	addDefaultDesign(elems[i])
+}
+elems = document.getElementsByClassName("itemcenter");
+for (let i = 0; i < elems.length; i++) {
+	addDefaultDesign(elems[i])
+}
