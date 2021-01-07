9385b406ad27e43b8c1423b88f30d833a7f7fd30
eliastheis <eliastheis.sg@gmail.com>
2021-01-07 23:48:50 +0100

    Fixed Footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -11,13 +11,11 @@ for (let i = 0; i < classes.length; i++) {
     elems = document.getElementsByClassName(classes[i]);
     for (let j = 0; j < elems.length; j++){
         try {
-            if (classes[i] == "bg-dark"){
-                elems[j].classList.remove("bg-dark");
-            }
-            if (classes[i] == "text-light"){
-                elems[j].classList.remove("text-light");
-            }
-            elems[j].style.backgroundColor = backgroundColor;
+            
+            elems[j].classList.remove("bg-dark");
+            elems[j].classList.remove("text-light");
+            
+            elems[j].style.backgroundColor = backgroundColorLight;
             elems[j].style.color = textColor;
             elems[j].style.borderColor = borderColor;
         } catch (e){}
@@ -25,5 +23,6 @@ for (let i = 0; i < classes.length; i++) {
 }
 
 elem = document.getElementById("page-footer");
-elem.style.backgroundColor = backgroundColor;
+elem.style.backgroundColor = backgroundColorLight;
 elem.style.color = textColor;
+
