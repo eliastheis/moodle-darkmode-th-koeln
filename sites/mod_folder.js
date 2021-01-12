cce575586312b0f40af17c802751e2963cf96c9b
eliastheis <eliastheis.sg@gmail.com>
2021-01-12 22:44:42 +0100

    Removed border in /mod/folder

---
+++ sites/mod_folder.js
@@ -0,0 +1,9 @@
+console.log('[DarkMode] loading mod_folder.js...');
+
+// tables (tbody)
+elems = document.getElementsByTagName('tbody');
+	for (let j = 0; j < elems.length; j++) {
+	try {
+	  elems[j].style.border = "none";
+	} catch (e) {}
+}
